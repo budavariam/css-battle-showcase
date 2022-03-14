6ad2c67ba4c6dd6843381435fa1e316d2a99f6fa
Mátyás Budavári <budavariam@gmail.com>
2022-03-14 21:54:04 +0100

    Add gh pages build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const menu = require("./utils-build/menu.js");
 module.exports = (phase, { defaultConfig }) => {
     return {
         ...defaultConfig,
+        basePath: "/css-battle-showcase",
         env: {
             menu: menu.get('_battles'),
         },
