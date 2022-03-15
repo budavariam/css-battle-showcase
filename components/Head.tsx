fec8f2d5034fd820a08d2d445384a0f2ae640b01
Mátyás Budavári <budavariam@gmail.com>
2022-03-15 14:15:58 +0100

    Do not add seo with missing image

--- components/Head.tsx
+++ components/Head.tsx
@@ -29,12 +29,16 @@ const Head = ({ customMeta }: { customMeta?: MetaProps }): JSX.Element => {
       />
       <meta property="og:description" content={meta.description} />
       <meta property="og:title" content={meta.title} />
-      <meta property="og:image" content={meta.image} />
-      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@budavari" />
       <meta name="twitter:title" content={meta.title} />
       <meta name="twitter:description" content={meta.description} />
-      <meta name="twitter:image" content={meta.image} />
+      {meta.image && (
+        <>
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta property="og:image" content={meta.image} />
+          <meta name="twitter:image" content={meta.image} />
+        </>
+      )}
       {meta.date && (
         <meta property="article:published_time" content={meta.date} />
       )}
