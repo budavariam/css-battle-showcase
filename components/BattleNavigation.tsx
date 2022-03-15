034b52869a8b2227ce5649c6697d050776fe213f
Mátyás Budavári <budavariam@gmail.com>
2022-03-15 10:37:09 +0100

    Fix home page button

--- components/BattleNavigation.tsx
+++ components/BattleNavigation.tsx
@@ -12,7 +12,7 @@ const BattleNavigation = () => {
                 <ul className="space-y-2">
                     <li>
                         <a
-                            href="/"
+                            href={`${process.env.basePath}/`}
                             className="flex items-center p-2 w-full text-base font-normal text-gray-900 rounded-lg transition duration-75 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
                         >Home</a>
                     </li>
